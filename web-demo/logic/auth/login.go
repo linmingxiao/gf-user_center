284868d1dd948a6433b5777baeef1ce1ffb524cc
mxlin <lmx_dlut@126.com>
2022-08-03 10:45:30 +0800

    登录接口

--- web-demo/logic/auth/login.go
+++ web-demo/logic/auth/login.go
@@ -1,9 +1,11 @@
 package auth
 
 import (
+	"gf-example/web-demo/cf"
 	"github.com/qinchende/gofast/fst"
 	"github.com/qinchende/gofast/logx"
 	"github.com/qinchende/gofast/sdx"
+	"github.com/qinchende/gofast/skill/jsonx"
 )
 
 func BeforeLogin(c *fst.Context) {
@@ -16,15 +18,46 @@ func BeforeLogin(c *fst.Context) {
 func LoginByAccPass(c *fst.Context) {
 	// 模拟验证登录，写入 user_id
 	account := c.GetString("account")
-	pass := c.GetString("pass")
+	password := c.GetString("password")
 
-	if account == "admin" && pass == "abc" {
-		sdx.DestroySession(c)
-		sdx.NewSession(c)
-		c.Sess.Set(sdx.MySessDB.GuidField, 111)
-		c.Sess.Save()
-		c.SucKV(fst.KV{})
+	var cus_parent_id string
+	if len(account) == 11 {
+		//手机号登录模式
+		cus_parent_id, _ = cf.R61501.Cli.HGet(c, `cus_parent_mobile`, account).Result()
+	} else if len(account) == 18 {
+		//身份证号登录模式
+		cus_parent_id, _ = cf.R61501.Cli.HGet(c, `cus_parent_card`, account).Result()
+	} else {
+		c.Fai(0, `账户名错误`, nil)
 		return
 	}
-	c.FaiStr("account and password error.")
+
+	userInfo, _ := cf.R61501.Cli.HGetAll(c, `cus_parent_`+cus_parent_id).Result()
+	pwd := userInfo[`hashed_password`]
+	if pwd != password {
+		c.Fai(0, `账户名密码错误`, nil)
+		return
+	}
+
+	wechat, _ := jsonx.GetMapFromString(userInfo[`wechat`])
+	fund, _ := jsonx.GetMapFromString(userInfo[`fund`])
+	base, _ := jsonx.GetMapFromString(userInfo[`base`])
+
+	println(base)
+
+	sdx.DestroySession(c)
+	sdx.NewSession(c)
+
+	pms := fst.KV{
+		sdx.MySessDB.GuidField: cus_parent_id,
+		`nickName`:             base[`nickname`],
+		`avatarUrl`:            base[`avatar_url`],
+		`wechatBind`:           wechat[`is_bind`],
+		`open_gm`:              fund[`identity_card`] != nil,
+	}
+
+	c.Sess.SetKV(pms)
+	c.Sess.Save()
+	c.SucKV(pms)
+	return
 }
